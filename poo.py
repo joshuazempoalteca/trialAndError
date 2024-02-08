78a0b2763d6e11a3f03ca704e344b7f6a73e1136
joshuazempoalteca <joshuazempoalteca@icloud.com>
2024-02-07 21:49:42 -0600

    I love if/elif/else statements!

---
+++ poo.py
@@ -0,0 +1,12 @@
+a = "This shit is awesome"
+b = "this is NOT awesome"
+c = 0
+d = 0
+
+c = input("Is this awesome? [Yes or No]")
+if c == "Yes":
+    print(a)
+elif c == "No":
+    print(b)
+else:
+    print("DONT YOU DARE FORGET HOW YOU BETRAYEEEEED ME")
